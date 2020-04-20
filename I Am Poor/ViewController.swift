bcd4a76bd078a8e7445dd8196732b2707b35aa31
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-04-21 00:48:37 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Geetansh Atrey on 21/04/20.
+//  Copyright © 2020 SpikedShark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
